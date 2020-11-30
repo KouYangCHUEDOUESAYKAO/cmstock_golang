834f7b10c7f278626b31136a00cac8a78e7d8e4b
KouYangCHUEDOUESAYKAO <kouyanghlubkoj@gmail.com>
2020-11-30 16:21:31 +0700

    ep1 - setup and static folder

---
+++ server.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	//"main/api"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.Static("/image", "./uploaded/images")
+
+	//api.Setup(router)
+	router.Run(":8081")
+}
