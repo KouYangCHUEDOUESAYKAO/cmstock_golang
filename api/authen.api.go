05d6a71cc3807d7aeffae79c7b75f2adb7836794
KouYangCHUEDOUESAYKAO <kouyanghlubkoj@gmail.com>
2020-11-30 17:22:31 +0700

    rest client vscode extension

--- api/authen.api.go
+++ api/authen.api.go
@@ -2,7 +2,8 @@ package api
 
 import "github.com/gin-gonic/gin"
 
-func SetupAuthenAPI(router *gin.Engine) {
+func SetupAuthenAPI(router *gin.Engine) 
+{
 	authenAPI := router.Group("/api/v2")
 	{
 		authenAPI.POST("/login", login)
@@ -12,10 +13,10 @@ func SetupAuthenAPI(router *gin.Engine) {
 
 // Login - login api
 func login(c *gin.Context) {
-	c.JSON(401, gin.H{"result": "login"})
+	c.JSON(200, gin.H{"result": "login"})
 }
 
 // Register - register api
 func register(c *gin.Context) {
-	c.JSON(401, gin.H{"result": "Register"})
+	c.JSON(200, gin.H{"result": "Register"})
 }
--- api/product.api.go
+++ api/product.api.go
@@ -5,17 +5,17 @@ import "github.com/gin-gonic/gin"
 func SetupProductAPI(router *gin.Engine) {
 	productAPI := router.Group("/api/v2")
 	{
-		productAPI.POST("/list", list)
-		productAPI.POST("/create", create)
+		productAPI.GET("/product", getProduct)
+		productAPI.POST("/product", createProduct)
 	}
 }
 
 // Llis - list api
-func list(c *gin.Context) {
-	c.JSON(401, gin.H{"result": "list"})
+func getProduct(c *gin.Context) {
+	c.JSON(200, gin.H{"result": "get product"})
 }
 
 // Create - create api
-func create(c *gin.Context) {
-	c.JSON(401, gin.H{"result": "create"})
+func createProduct(c *gin.Context) {
+	c.JSON(200, gin.H{"result": "create product"})
 }
