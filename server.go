1e5069f0d6c257342b1cc49606aa39b9819cc34b
KouYangCHUEDOUESAYKAO <kouyanghlubkoj@gmail.com>
2020-12-02 22:49:58 +0700

    deploment on heroku

--- server.go
+++ server.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"main/api"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,4 +14,13 @@ func main() {
 
 	api.Setup(router)
 	router.Run(":8081")
+
+	var port = os.Getenv("PORT")
+	if port == "" {
+		fmt.Println("No Port In Heroku")
+		router.Run()
+	} else {
+		fmt.Println("Environment Port : " + port)
+		router.Run(":" + port)
+	}
 }
